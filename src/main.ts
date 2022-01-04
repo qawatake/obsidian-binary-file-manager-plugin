ad5428ea69fa2b30a8e70af7431bda1a107461a5
qawatake <greenmackey@gmail.com>
2022-01-04 14:13:09 +0900

    remove redundant codes

--- src/main.ts
+++ src/main.ts
@@ -64,12 +64,6 @@ export default class BinaryFileManagerPlugin extends Plugin {
 					return;
 				}
 
-				// const metaDataFileName = this.uniquefyMetaDataFileName(
-				// 	this.generateMetaDataFileName(file as TFile)
-				// );
-				// const metaDataFilePath = `${this.settings.folder}/${metaDataFileName}`;
-
-				// await this.createMetaDataFile(metaDataFilePath, file as TFile);
 				await this.metaDataGenerator.create(file as TFile);
 				new Notice(`Meta data file of ${file.name} is created.`);
 				this.fileListAdapter.add(file.path);
@@ -92,112 +86,6 @@ export default class BinaryFileManagerPlugin extends Plugin {
 	}
 
 	// onunload() {}
-
-	// private async shouldCreateMetaDataFile(
-	// 	file: TAbstractFile
-	// ): Promise<boolean> {
-	// 	if (!(file instanceof TFile)) {
-	// 		return false;
-	// 	}
-
-	// 	const matchedExtension =
-	// 		this.fileExtensionManager.getExtensionMatchedBest(file.name);
-	// 	if (!matchedExtension) {
-	// 		return false;
-	// 	}
-
-	// 	if (this.fileListAdapter.has(file.path)) {
-	// 		return false;
-	// 	}
-
-	// 	return true;
-	// }
-
-	// private generateMetaDataFileName(file: TFile): string {
-	// 	const metaDataFileName = `${this.formatter.format(
-	// 		this.settings.filenameFormat,
-	// 		file.path,
-	// 		file.stat.ctime
-	// 	)}.md`;
-	// 	return metaDataFileName;
-	// }
-
-	// private uniquefyMetaDataFileName(metaDataFileName: string): string {
-	// 	const metaDataFilePath = normalizePath(
-	// 		`${this.settings.folder}/${metaDataFileName}`
-	// 	);
-	// 	if (this.app.vault.getAbstractFileByPath(metaDataFilePath)) {
-	// 		return `CONFLICT-${moment().format(
-	// 			'YYYY-MM-DD-hh-mm-ss'
-	// 		)}-${metaDataFileName}`;
-	// 	} else {
-	// 		return metaDataFileName;
-	// 	}
-	// }
-
-	// private async createMetaDataFile(
-	// 	metaDataFilePath: string,
-	// 	binaryFile: TFile
-	// ): Promise<void> {
-	// 	const templateContent = await this.fetchTemplateContent();
-
-	// 	// process by Templater
-	// 	const templaterPlugin = await this.getTemplaterPlugin();
-	// 	if (!(this.settings.useTemplater && templaterPlugin)) {
-	// 		this.app.vault.create(
-	// 			metaDataFilePath,
-	// 			this.formatter.format(
-	// 				templateContent,
-	// 				binaryFile.path,
-	// 				binaryFile.stat.ctime
-	// 			)
-	// 		);
-	// 	} else {
-	// 		const targetFile = await this.app.vault.create(
-	// 			metaDataFilePath,
-	// 			''
-	// 		);
-
-	// 		try {
-	// 			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-	// 			// @ts-ignore
-	// 			const content = await templaterPlugin.templater.parse_template(
-	// 				{ target_file: targetFile, run_mode: 4 },
-	// 				this.formatter.format(
-	// 					templateContent,
-	// 					binaryFile.path,
-	// 					binaryFile.stat.ctime
-	// 				)
-	// 			);
-	// 			this.app.vault.modify(targetFile, content);
-	// 		} catch (err) {
-	// 			new Notice(
-	// 				'ERROR in Binary File Manager Plugin: failed to connect to Templater. Your Templater version may not be supported'
-	// 			);
-	// 			console.log(err);
-	// 		}
-	// 	}
-	// }
-
-	// private async fetchTemplateContent(): Promise<string> {
-	// 	if (this.settings.templatePath === '') {
-	// 		return DEFAULT_TEMPLATE_CONTENT;
-	// 	}
-	// 	const templateFile = this.app.vault.getAbstractFileByPath(
-	// 		this.settings.templatePath
-	// 	);
-	// 	if (!(templateFile instanceof TFile)) {
-	// 		console.log(this.settings.templatePath);
-	// 		new Notice(`Template file ${templateFile} is invalid`);
-	// 		return DEFAULT_TEMPLATE_CONTENT;
-	// 	}
-	// 	return await this.app.vault.read(templateFile);
-	// }
-
-	// private async getTemplaterPlugin(): Promise<Plugin | undefined> {
-	// 	const app = this.app as UncoveredApp;
-	// 	return app.plugins.plugins[TEMPLATER_PLUGIN_NAME];
-	// }
 
 	async loadSettings() {
 		this.settings = Object.assign(
