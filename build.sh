93202febc56217dc2fb2fffc9e83910c94273c88
willjasen <brandon@willjasen.com>
2026-04-21 17:53:56 -0400

    fix pathing issue

--- build.sh
+++ build.sh
@@ -5,24 +5,26 @@
 
 # variables
 SOURCE_DIR="/Users/willjasen/GitHub/obsidian-binary-file-manager-plugin";
-PLUGIN_DIR="/Users/willjasen/Obsidian/willjasen/.obsidian/plugins/obsidian-binary-file-manager-plugin";
+PLUGIN_DIR="/Users/willjasen/Library/Mobile Documents/iCloud~md~obsidian/Documents/willjasen/.obsidian/plugins/obsidian-binary-file-manager-plugin";
 FILES=( "main.js" "manifest.json" "styles.css" );
 
-# Create the directory if needed
-if [ -d $PLUGIN_DIR ]; then 
+
+# Create the directory if needed (handle spaces and parents)
+if [ -d "$PLUGIN_DIR" ]; then 
   echo "Plugin directory already exists.";
 else
-  echo "Creating plugin directory at $PLUGIN_DIR"; mkdir $PLUGIN_DIR;
+  echo "Creating plugin directory at $PLUGIN_DIR"; mkdir -p "$PLUGIN_DIR";
 fi
 
 # Go to the source directory and build this plugin
-cd $SOURCE_DIR;
+
+cd "$SOURCE_DIR";
 npm i --silent; npm run build > /dev/null 2>&1;
 
 # Copy built files into the plugin directory
 for file in "${FILES[@]}"
 do
   echo "Copying file: $file --> $PLUGIN_DIR/$file";
-  cp $SOURCE_DIR/$file $PLUGIN_DIR/$file;
+  cp "$SOURCE_DIR/$file" "$PLUGIN_DIR/$file";
 done
 echo "All files copied!";
